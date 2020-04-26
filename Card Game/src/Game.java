b622c61fc9deb114877499b05d5ba8350893246a
JuliaVisolli <julia_viso@hotmail.com>
2020-04-25 21:13:43 -0300

    implementação do input

--- Card Game/src/Game.java
+++ Card Game/src/Game.java
@@ -5,26 +5,28 @@ import java.util.Scanner;
 
 public class Game {
 	static Scanner entradaTeclado = new Scanner(System.in);
+	
+	
 
 	public static void main(String[] args) {
 
 		@SuppressWarnings("resource")
 		Scanner entraTeclado = new Scanner(System.in);
 
-		Personagem arqueiro = new Arqueiro();
-		arqueiro.setNome("Arqueiro");
-		arqueiro.setEnergia(10);
-		arqueiro.setVivo(true);
+		Personagem humano = new Humano(null, 0, false);
+		humano.setNome("Arqueiro");
+		humano.setEnergia(10);
+		humano.setVivo(true);
 
-		Personagem dragao = new Dragao();
-		dragao.setNome("Dragï¿½o");
-		dragao.setEnergia(10);
-		dragao.setVivo(true);
+		Personagem dragoes = new Dragoes(null, 0, false);
+		dragoes.setNome("Dragão");
+		dragoes.setEnergia(10);
+		dragoes.setVivo(true);
 
-		Personagem orcDaMontanha = new OrcDaMontanha();
-		orcDaMontanha.setNome("Orc do Deserto");
-		orcDaMontanha.setEnergia(10);
-		orcDaMontanha.setVivo(true);
+		Personagem orcs = new Orcs(null, 0, false);
+		orcs.setNome("Orc do Deserto");
+		orcs.setEnergia(10);
+		orcs.setVivo(true);
 
 		Personagem orcDoDeserto = new OrcDoDeserto();
 		orcDoDeserto.setNome("Orc da Montanha");
@@ -34,7 +36,7 @@ public class Game {
 		System.out.println("Escolha o seu personagem");
 		System.out.println();
 		System.out.println("Digite 1 para Arqueiro");
-		System.out.println("Digite 2 para Dragï¿½o");
+		System.out.println("Digite 2 para Dragão");
 		System.out.println("Digite 3 para Orc do Deserto");
 		System.out.println("Digite 4 para Orc da Montanha");
 		System.out.println();
@@ -46,13 +48,13 @@ public class Game {
 
 		switch (jogadorEscolhido) {
 		case 1:
-			personagem1 = arqueiro;
+			personagem1 = humano;
 			break;
 		case 2:
-			personagem1 = dragao;
+			personagem1 = dragoes;
 			break;
 		case 3:
-			personagem1 = orcDaMontanha;
+			personagem1 = orcs;
 			break;
 		case 4:
 			personagem1 = orcDoDeserto;
@@ -66,21 +68,21 @@ public class Game {
 		System.out.println("");
 
 		List<Personagem> personagensAdversario = new ArrayList<Personagem>();
-		personagensAdversario.add(arqueiro);
-		personagensAdversario.add(dragao);
+		personagensAdversario.add(humano);
+		personagensAdversario.add(dragoes);
+		personagensAdversario.add(orcs);
 		personagensAdversario.add(orcDoDeserto);
-		personagensAdversario.add(orcDaMontanha);
 		
 		Random rand = new Random();
 		Personagem personagem2 = personagensAdversario.get(rand.nextInt(personagensAdversario.size()));
 
-		System.out.println("O personagem adversï¿½rio: " + personagem2.getNome());
+		System.out.println("O personagem adversário: " + personagem2.getNome());
 		
 		List<Personagem> personagens = new ArrayList<Personagem>();
-		personagens.add(arqueiro);
-		personagens.add(dragao);
+		personagens.add(humano);
+		personagens.add(dragoes);
+		personagens.add(orcs);
 		personagens.add(orcDoDeserto);
-		personagens.add(orcDaMontanha);
 
 		System.out.println();
 		System.out.println("Digite 1 para desferir o ataque:");
@@ -104,17 +106,17 @@ public class Game {
 			personagem2.recebeAtaque(forcaAtaque, 0);
 			System.out.println("Seu Dano: " + forcaAtaque);
 			personagem1.recebeAtaque(ataqueDesferidoSegundoPerson, 0);
-			System.out.println("Dano do adversï¿½rio: " + ataqueDesferidoSegundoPerson);
+			System.out.println("Dano do adversário: " + ataqueDesferidoSegundoPerson);
 			
-			System.out.println("Vida personagem 1:" + personagem1.getEnergia());
-			System.out.println("Vida personagem 2:" + personagem2.getEnergia());
+			System.out.println("Vida personagem 1: " + personagem1.getEnergia());
+			System.out.println("Vida personagem 2: " + personagem2.getEnergia());
 		}
 		if (personagem2.getEnergia() <= 0) {
 
-			System.out.println("O vencedor ï¿½: " + personagem1.getNome());
+			System.out.println("O vencedor é: " + personagem1.getNome());
 		} else {
 
-			System.out.println("O vencedor ï¿½: " + personagem2.getNome());
+			System.out.println("O vencedor é: " + personagem2.getNome());
 		}
 	}
 
